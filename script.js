e08ddb9510e42773cad2a247a8b2bba5a4b7f895
Gaji <islamgajinajrul@gmail.com>
2026-02-23 00:10:49 +0100

    Interview and Rejected Thriving and delegation done. Count is not working as expected

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ let interviewList = [];
 let rejectedList = [];
 let currentValue ='all';
 
-let total =document.getElementById("total");
-let nrOfInterviews = document.getElementById("numOfInterview");
-let nrOfRejected = document.getElementById("numOfRejected");
-let nrOfCart = document.getElementById("numOfCart");
-let numOfJobs = document.getElementById("numOfJobs");
-const main = document.getElementById("main");
-const filterJobs = document.getElementById("filterJobs");
+let total =document.getElementById('total');
+let nrOfInterviews = document.getElementById('numOfInterview');
+let nrOfRejected = document.getElementById('numOfRejected');
 
-let btnInterviewSelect = document.getElementById("btnInterviewSelect");
-let btnRejectedSelect = document.getElementById("btnRejectedSelect");
-let jobStatus = document.getElementById("jobStatus");
+
+let allJobs = document.getElementById('allJobs');
+let numOfJobs = document.getElementById('numOfJobs');
+const main = document.getElementById('main');
+const filterJobs = document.getElementById('filterJobs');
+
+//const btnInterviewSelect = document.getElementById('btnInterviewSelect');
+//const btnRejectedSelect = document.getElementById('btnRejectedSelect');
+const jobStatus = document.getElementById('jobStatus');
 //console.log(main);
 //console.log(nrOfCart);
 //interviewList.push("interview1");
@@ -23,8 +25,8 @@ const btnRejected = document.getElementById("btnRejected");
 
 // funcrion to update the total number of applications, interviews, and rejections
 function updateTotal() {
-    total.innerText = nrOfCart.children.length - 1;
-    numOfJobs.innerHTML = nrOfCart.children.length - 1;
+    total.innerText = allJobs.children.length ;
+    numOfJobs.innerHTML = allJobs.children.length ;
     nrOfInterviews.innerHTML = interviewList.length;
     nrOfRejected.innerHTML = rejectedList.length;
     //console.log(totalApplications);
@@ -40,89 +42,171 @@ function toggelColor(id){
     btnAll.classList.remove('bg-[#3B82F6]','text-white');
     btnInterview.classList.remove('bg-white','text-white');
     btnRejected.classList.remove('bg-white','text-white');
+
     // add the active class to the clicked button
     btnAll.classList.add('bg-white','text-black');
     btnInterview.classList.add('bg-white','text-black');
     btnRejected.classList.add('bg-white','text-black');
 
     const selected = document.getElementById(id);
+
     currentValue = id;
     console.log(currentValue);
+
     selected.classList.remove('bg-white');
     selected.classList.add('bg-[#3B82F6]','text-white');
 
     if(id == 'btnInterview'){
-        nrOfCart.classList.add('hidden');
+        allJobs.classList.add('hidden');
         filterJobs.classList.remove('hidden');
+        renderInterviewed();
+    }else if(id == 'btnAll'){
+        allJobs.classList.remove('hidden');
+        filterJobs.classList.add('hidden');
+
+    }else if(id == 'btnRejected'){
+        allJobs.classList.add('hidden');
+        filterJobs.classList.remove('hidden');
+        renderRejected();
     }
 }
 
-main.addEventListener("click",function (event) {
+//delegation event for interview and rejected buttons
 
-    //console.log(event.target.id.includes("btnInterviewSelect"));
+main.addEventListener('click',function (event) {
 
-    if(event.target.id.includes("btnInterviewSelect")){
+    
+    if(event.target.classList.contains('btnInterviewSelect')){
 
-        const cartParent = event.target.parentNode.parentNode
+        const jobsParent = event.target.parentNode.parentNode;
+        //console.log(jobsParent);
 
-        const companyTitle = cartParent.querySelector('#companyTitle').innerText;
-        //console.log(companyTitle);
-        const jobTitle = cartParent.querySelector('#jobTitle').innerText;
-        const salary = cartParent.querySelector('#salary').innerText;
-        const jobDes = cartParent.querySelector('#jobDes').innerText;
-        const jobStatus = cartParent.querySelector('#jobStatus').placeholder;
-        console.log(companyTitle, jobTitle, salary, jobDes, jobStatus);
+        const companyName = jobsParent.querySelector('#companyName').innerText;
+        const position = jobsParent.querySelector('#position').innerText;
+        const salary = jobsParent.querySelector('#salary').innerText;
+        const jobStatus = jobsParent.querySelector('#jobStatus').innerText;
+        const jobDes = jobsParent.querySelector('#jobDes').innerText;
+
+        //console.log(companyName, position, salary, jobStatus, jobDes);
+
+        jobsParent.querySelector('#jobStatus').innerText = 'Interviewed';
 
         const jobInfo = {
-            companyTitle: companyTitle,
-            jobTitle: jobTitle,
-            salary: salary,
-            jobStatus: jobStatus,
-            jobDes: jobDes
+            companyName,
+            position,
+            salary,
+            jobStatus: 'Interviewed',
+            jobDes
             
         }
-        //console.log(jobInfo);
-        const jobExist = interviewList.find(job => job.companyTitle === jobInfo.companyTitle);
-        cartParent.querySelector('#jobStatus').value = "Interviewed";
+        
+        const jobExist = interviewList.find(job => job.companyName == jobInfo.companyName);
+        
         if(!jobExist){
             interviewList.push(jobInfo);
         }
 
-        rejectedList = rejectedList.filter(job => job.companyTitle !== jobInfo.companyTitle);
+        rejectedList = rejectedList.filter(job => job.companyName != jobInfo.companyName);
 
-        if(currentValue === "btnRejected"){
-            rejectedList.push(jobInfo);
+        if(currentValue == 'btnRejected'){
+            renderRejected();
         }
-        renderInterviewed();
+        updateTotal();
+    }else if(event.target.classList.contains('btnRejectedSelect')){
+
+        const jobsParent = event.target.parentNode.parentNode;
+        //console.log(jobsParent);
+        const companyName = jobsParent.querySelector('#companyName').innerText;
+        const position = jobsParent.querySelector('#position').innerText;
+        const salary = jobsParent.querySelector('#salary').innerText;
+        const jobStatus = jobsParent.querySelector('#jobStatus').innerText;
+        const jobDes = jobsParent.querySelector('#jobDes').innerText;
+
+        jobsParent.querySelector('#jobStatus').innerText = 'Rejected';
+
+        const jobInfo = {
+            companyName,
+            position,
+            salary,
+            jobStatus: 'Rejected',
+            jobDes
+    }
+    const jobExist = rejectedList.find(job => job.companyName == jobInfo.companyName);
+    if(!jobExist){
+        rejectedList.push(jobInfo);
+    }
+    interviewList = interviewList.filter(job => job.companyName != jobInfo.companyName);
+    if(currentValue == 'btnRejected'){
+        renderRejected();
+    }
+    updateTotal();
     }
 })
 
+//creat element for rejected button
 function renderInterviewed(){
-    filterJobs.innerHTML = "";
+    filterJobs.innerHTML = '';
+
     for(let interview of interviewList){
         console.log(interview);
-        const div = document.createElement("div");
-        div.classList.add("shadow-md", "rounded-lg", "bg-white", "min-h-[300px]", "px-6", "py-6");
+
+        let div = document.createElement('div');
+        div.className = 'jobs shadow-md rounded-lg bg-white min-h-[300px] px-6 py-6';
         div.innerHTML = `
             
-                <div class="flex justify-between" >
+            <div class="flex justify-between" >
                     <div>
                         <h3 id="companyTitle" class="text-[18px] font-bold text-black mb-1">Mobile First Corp</h3>
                         <p id="jobTitle" class="text-[18px] text-gray-500">React Native Developer</p>
                     </div>
-                    <div id="btnRemove">
-                        <button onclick="removeJob(this)" class="w-8 h-8 bg-red-500 text-white rounded-full">X</button>
+                    <div >
+                        <button  class="removeJobs w-8 h-8 bg-red-500 text-white rounded-full">X</button>
                     </div>
                 </div>
                     <p id="salary" class="my-5">Remote • Full-time •$130,000 - $175,000</p>
                 <div>
-                    <input id="jobStatus" class="px-3 py-2 bg-[#EEF4FF] w-35" type="text" disabled placeholder="Not applicable">
+                    <p id="jobStatus" class="px-3 py-2 bg-[#EEF4FF] w-35" type="text" >${interview.jobStatus}</p>
                     <p id="jobDes" class="mt-2 mb-5">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
                 </div>
                 <div class="flex gap-5">
                     <button id="btnInterviewSelect" class="px-3 py-2 border-2 border-green-300 text-green-500 rounded-[5px]">INTERVIEW</button>
                     <button id="btnRejectedSelect" class="px-3 py-2 border-2 border-red-300 text-red-500 rounded-[5px]">REJECTED</button>
                 </div>
+            </div>
+        `;
+        filterJobs.appendChild(div);
+    }
+}
+
+function renderRejected(){
+    filterJobs.innerHTML = '';
+
+    for(let rejected of rejectedList){
+        console.log(rejected);
+
+        let div = document.createElement('div');
+        div.className = ' jobs shadow-md rounded-lg bg-white min-h-[300px] px-6 py-6';
+        div.innerHTML = `
+            
+            <div class="flex justify-between" >
+                    <div>
+                        <h3 id="companyTitle" class="text-[18px] font-bold text-black mb-1">Mobile First Corp</h3>
+                        <p id="jobTitle" class="text-[18px] text-gray-500">React Native Developer</p>
+                    </div>
+                    <div >
+                        <button  class="removeJobs w-8 h-8 bg-red-500 text-white rounded-full">X</button>
+                    </div>
+                    </div>
+                        <p id="salary" class="my-5">Remote • Full-time •$130,000 - $175,000</p>
+                    <div>
+                        <p id="jobStatus" class="px-3 py-2 bg-[#EEF4FF] w-35" type="text" >${rejected.jobStatus}</p>
+                        <p id="jobDes" class="mt-2 mb-5">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+                    </div>
+                <div class="flex gap-5">
+                    <button id="btnInterviewSelect" class="px-3 py-2 border-2 border-green-300 text-green-500 rounded-[5px]">INTERVIEW</button>
+                    <button id="btnRejectedSelect" class="px-3 py-2 border-2 border-red-300 text-red-500 rounded-[5px]">REJECTED</button>
+                </div>
+            </div>
         `;
         filterJobs.appendChild(div);
     }
