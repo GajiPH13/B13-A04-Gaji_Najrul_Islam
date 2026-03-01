4e791266e334e11d371d8a7083fa6e6399b33a61
GajiPH13 <islamgajinajrul@gmail.com>
2026-03-01 14:36:54 +0100

    toggel between two btn

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let numOfJobs = document.getElementById('numOfJobs');
 
 const allJobs = document.getElementById('allJobs');
 const mainSection = document.getElementById('mainSection');
+
 const filterJobs = document.getElementById('filterJobs');
 
 const btnAll = document.getElementById("btnAll");
@@ -28,31 +29,29 @@ const deletJob = document.getElementById('btnRemove');
 function updateTotal() {
     total.innerText = allJobs.children.length;
     numOfJobs.innerText = allJobs.children.length;
-    
+
     nrOfInterviews.innerText = interviewList.length;
     nrOfRejected.innerText = rejectedList.length;
-   
- 
 }
 
- updateTotal();
+updateTotal();
 
- // function to toggle the color of the buttons and filter the jobs based on the selected button
+// function to toggle the color of the buttons and filter the jobs based on the selected button
 
-function toggelColor(id){
-    
+function toggelColor(id) {
+
     // remove the active class from all buttons
 
-    btnAll.classList.remove('bg-[#3B82F6]','text-white');
-    btnInterview.classList.remove('bg-white','text-white');
-    btnRejected.classList.remove('bg-white','text-white');
+    btnAll.classList.remove('bg-[#3B82F6]', 'text-white');
+    btnInterview.classList.remove('bg-white', 'text-white');
+    btnRejected.classList.remove('bg-white', 'text-white');
 
     // add the active class to the clicked button
 
-    btnAll.classList.add('bg-white','text-black');
-    btnInterview.classList.add('bg-white','text-black');
-    btnRejected.classList.add('bg-white','text-black');
-    
+    btnAll.classList.add('bg-white', 'text-black');
+    btnInterview.classList.add('bg-white', 'text-black');
+    btnRejected.classList.add('bg-white', 'text-black');
+
     // to put getElementById in a variable to avoid repetition and make the code cleaner
 
     const selected = document.getElementById(id);
@@ -61,14 +60,14 @@ function toggelColor(id){
     console.log(currentValue);
 
     selected.classList.remove('bg-white');
-    selected.classList.add('bg-[#3B82F6]','text-white');
+    selected.classList.add('bg-[#3B82F6]', 'text-white');
 
     // when the Interwiew button is clicked, hide all jobs and 
     // show only the interviewed jobs, and update the number of jobs 
     // in the interview list and the total number of jobs, and display 
     // the no jobs toast message if there are no jobs in the interview list
 
-    if(id == 'btnInterview'){
+    if (id == 'btnInterview') {
         allJobs.classList.add('hidden');
         filterJobs.classList.remove('hidden');
         renderInterviewed();
@@ -77,44 +76,49 @@ function toggelColor(id){
         numOfJobs.innerText = `${interviewList.length} of ${allJobs.children.length} `;
 
         // display the no jobs toast message if there are no jobs in the interview list
-        if(interviewList.length == 0){
+        if (interviewList.length == 0) {
             noJobs();
         }
-        
-    // when the All button is clicked, show all jobs and hide the filtered jobs, 
-    // and update the number of jobs in the total list
 
-    }else if(id == 'btnAll'){
+        // when the All button is clicked, show all jobs and hide the filtered jobs, 
+        // and update the number of jobs in the total list
+
+    } else if (id == 'btnAll') {
         allJobs.classList.remove('hidden');
         filterJobs.classList.add('hidden');
-        numOfJobs.innerText = allJobs.children.length;
-        
-        
-    // when the Rejected button is clicked, hide all jobs and show only the rejected jobs,
-    // and update the number of jobs in the rejected list and the total number of jobs, 
-    // and display the no jobs toast message if there are no jobs in the rejected list
 
-    }else if(id == 'btnRejected'){
+        numOfJobs.innerText = allJobs.children.length;
+
+
+        // when the Rejected button is clicked, hide all jobs and show only the rejected jobs,
+        // and update the number of jobs in the rejected list and the total number of jobs, 
+        // and display the no jobs toast message if there are no jobs in the rejected list
+
+    } else if (id == 'btnRejected') {
         allJobs.classList.add('hidden');
         filterJobs.classList.remove('hidden');
         renderRejected();
-        numOfJobs.innerText = `${rejectedList.length} of ${allJobs.children.length}`;
-        if(rejectedList.length == 0){
+
+        numOfJobs.innerText = `${rejectedList.length} of ${allJobs.children.length} `;
+        
+        if (rejectedList.length == 0) {
             noJobs();
         }
+        
+        
     }
 }
 
 //Delegation event for interview and rejected buttons
 
-mainSection.addEventListener('click',function (event) {
+mainSection.addEventListener('click', function (event) {
 
-    
-    if(event.target.classList.contains('btnInterviewSelect')){
+
+    if (event.target.classList.contains('btnInterviewSelect')) {
 
         // put the parent node of the button in a variable to avoid repetition 
         const jobsParent = event.target.parentNode.parentNode;
-        
+        //console.log(jobsParent)
         // get the job information from the parent node and put it in a variable to avoid repetition 
         const companyName = jobsParent.querySelector('.companyName').innerText;
         const position = jobsParent.querySelector('.position').innerText;
@@ -125,33 +129,33 @@ mainSection.addEventListener('click',function (event) {
         //creat a list of array of object to store in interviewList
         jobsParent.querySelector('.jobStatus').innerText = 'Interviewed';
 
-            const jobInfo = {
-                companyName,
-                position,
-                salary,
-                jobStatus: 'Interviewed',
-                jobDes
-                
-            }
-        // check if the job already exist in the interview list, if not push it to the interview list
-            const jobExist = interviewList.find(job => job.companyName == jobInfo.companyName);
-        
-            if(!jobExist){
-                interviewList.push(jobInfo);
-            }
-        // filter the rejected list to remove the job that is added to the interview list
-            rejectedList = rejectedList.filter(job => job.companyName !== jobInfo.companyName);
+        const jobInfo = {
+            companyName,
+            position,
+            salary,
+            jobStatus: 'Interviewed',
+            jobDes
 
-            if(currentValue == 'btnRejected'){
-                //
-                renderRejected();
-                //
-            }
+        }
+        // check if the job already exist in the interview list, if not push it to the interview list
+        const jobExist = interviewList.find(job => job.companyName == jobInfo.companyName);
+
+        if (!jobExist) {
+            interviewList.push(jobInfo);
+        }
+        // filter the rejected list to remove the job that is added to the interview list
+        rejectedList = rejectedList.filter(job => job.companyName !== jobInfo.companyName);
+
+        if (currentValue == 'btnRejected') {
+            //
+            renderRejected();
+            //
+        }
 
             updateTotal();
         //
 
-    }else if(event.target.classList.contains('btnRejectedSelect')){
+    } else if (event.target.classList.contains('btnRejectedSelect')) {
 
         const jobsParent = event.target.parentNode.parentNode;
         //console.log(jobsParent);
@@ -193,7 +197,7 @@ mainSection.addEventListener('click',function (event) {
         
         event.target.parentNode.parentNode.parentNode.remove();
         //console.log(event.target.parentNode.parentNode.parentNode);
-        filterJobs.classList.add('hidden');
+        //filterJobs.classList.add('hidden');
         //interviewList.add('hidden');
 
         updateTotal();
@@ -203,16 +207,18 @@ mainSection.addEventListener('click',function (event) {
             
 
     
-    
-    
-    
+   
+     
+
 })
 
+
+
 //creat element for interview button
-function renderInterviewed(){
+function renderInterviewed() {
     filterJobs.innerHTML = '';
 
-    for(let interview of interviewList){
+    for (let interview of interviewList) {
         //console.log(interview);
 
         let div = document.createElement('div');
@@ -221,11 +227,11 @@ function renderInterviewed(){
             
             <div class="flex justify-between" >
                     <div>
-                        <h3  class=" companyTitle text-[18px] font-bold text-black mb-1">${interview.companyName}</h3>
-                        <p  class="jobTitle text-[18px] text-gray-500">${interview.position}</p>
+                        <h3  class=" companyName text-[18px] font-bold text-black mb-1">${interview.companyName}</h3>
+                        <p  class="position text-[18px] text-gray-500">${interview.position}</p>
                     </div>
                     <div >
-                        <button  class="removeJobs w-8 h-8 bg-red-500 text-white rounded-full">X</button>
+                        <button  class="btnRemove w-8 h-8 bg-red-500 text-white rounded-full">X</button>
                     </div>
                     </div>
                         <p  class="salary my-5">Remote • Full-time • $130,000 - $175,000>${interview.salary}</p>
@@ -244,23 +250,23 @@ function renderInterviewed(){
 }
 
 //creat element for rejected button
-function renderRejected(){
+function renderRejected() {
     filterJobs.innerHTML = '';
 
-    for(let rejected of rejectedList){
+    for (let rejected of rejectedList) {
         //console.log(rejected.length);
 
         let div = document.createElement('div');
-        div.className = ' jobs shadow-md rounded-lg bg-white min-h-[300px] px-6 py-6';
+        div.className = ' jobs flex-col gap-4 shadow-md rounded-lg bg-white min-h-[300px] mb-5 px-6 py-6';
         div.innerHTML = `
             
             <div class="flex justify-between" >
                     <div>
-                        <h3  class=" companyTitle text-[18px] font-bold text-black mb-1">${rejected.companyName}</h3>
-                        <p  class="jobTitle text-[18px] text-gray-500">${rejected.position}</p>
+                        <h3  class=" companyName text-[18px] font-bold text-black mb-1">${rejected.companyName}</h3>
+                        <p  class="position text-[18px] text-gray-500">${rejected.position}</p>
                     </div>
                     <div >
-                        <button  class="removeJobs w-8 h-8 bg-red-500 text-white rounded-full">X</button>
+                        <button  class="btnRemove w-8 h-8 bg-red-500 text-white rounded-full">X</button>
                     </div>
                     </div>
                         <p  class="salary my-5">Remote • Full-time • $130,000 - $175,000>${rejected.salary}</p>
@@ -280,7 +286,7 @@ function renderRejected(){
 
 // function to display the no jobs toast message when there are no jobs in the interview or rejected list
 
-function noJobs(){
+function noJobs() {
     filterJobs.innerHTML = '';
     let div = document.createElement('div');
     div.className = "flex flex-col items-center  justify-center shadow-md rounded-lg bg-white min-h-[300px] px-6 py-6 "
@@ -294,6 +300,6 @@ function noJobs(){
         </div>
     `
     filterJobs.appendChild(div);
-   
+
 }
 
